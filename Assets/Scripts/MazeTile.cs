1e62dee0d6e78ed2501b2c9278bad4926d959b05
Davis Pham <arcentalminitte@gmail.com>
2018-10-25 16:12:26 -0700

    seperated walls

--- Assets/Scripts/MazeTile.cs
+++ Assets/Scripts/MazeTile.cs
@@ -1,9 +1,29 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class MazeTile : MonoBehaviour {
 
+	public Transform wallParent;
+
 	/// <summary>
-	/// Array of walls of the tile
+	/// list of walls of the tile
 	/// </summary>
-	public GameObject[] walls;
+	public List<GameObject> walls;
+
+	/// <summary>
+	/// Awake is called when the script instance is being loaded.
+	/// </summary>
+	void Awake() {
+		walls = new List<GameObject>();
+	}
+
+	/// <summary>
+	/// Adds a wall of the tile
+	/// </summary>
+	/// <param name="wall"></param>
+	public void AddWall(GameObject wall) {
+		walls.Add(wall);
+
+		wall.transform.SetParent(wallParent, false);
+	}
 }
