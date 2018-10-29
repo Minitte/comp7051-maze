900fb041519d6c751aa94820a08ecd9e94aa7698
Anthony Wong <awong6201@gmail.com>
2018-10-28 23:43:10 -0700

    Implemented a toggleable fog effect

---
+++ Assets/FogShader.shader
@@ -0,0 +1,66 @@
+﻿/*
+	References:
+	http://willychyr.com/2013/11/unity-shaders-depth-and-normal-textures/
+*/
+
+Shader "Custom/FogShader" {
+	Properties
+	{
+		_RedAlpha("Red Alpha", Float) = 0.5
+		_GreenAlpha("Blue Alpha", Float) = 0.5
+		_BlueAlpha("Green Alpha", Float) = 0.5
+		_MainTex("White Texture", 2D) = "white" {}
+	}
+	SubShader
+	{
+		Pass
+		{
+
+		CGPROGRAM
+		#pragma vertex vert
+		#pragma fragment frag
+		#include "UnityCG.cginc"
+
+		sampler2D _CameraDepthTexture;
+		
+		// Container for information that is passed from vertex to fragment
+		struct v2f
+		{
+			float2 uv : TEXCOORD0;
+			float4 pos : SV_POSITION;
+			float4 scrPos : TEXCOORD1;
+		};
+
+		sampler2D _MainTex;
+
+		// Vertex Shader
+		v2f vert(appdata_full v)
+		{
+			v2f output;
+			output.uv = v.texcoord.xy; // Texture map
+			output.pos = UnityObjectToClipPos(v.vertex); // Vertex's world position
+			output.scrPos = ComputeScreenPos(output.pos); // Vertex's screen position
+			return output;
+		}
+
+		// Fragment Shader
+		float4 frag(v2f input) : COLOR
+		{
+			// Value that increases depending on how far away the object is
+			float depthValue = Linear01Depth(tex2Dproj(_CameraDepthTexture, UNITY_PROJ_COORD(input.scrPos)).r);
+			float4 texColour = tex2D(_MainTex, input.uv); // The actual colours of the texture
+
+			// Calculate final colours, further objects will appear more white
+			float4 depthColour;
+			depthColour.r = texColour.r + depthValue;
+			depthColour.g = texColour.b + depthValue;
+			depthColour.b = texColour.g + depthValue;
+			depthColour.a = 1;
+
+			return depthColour;
+		}
+		ENDCG
+		}
+	}
+		FallBack "Diffuse"
+}
---
+++ Assets/Scripts/Fog.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// This is a class for displaying the fog effect.
+/// </summary>
+[ExecuteInEditMode]
+public class Fog : MonoBehaviour {
+
+    /// <summary>
+    /// The fog material.
+    /// </summary>
+    public Material fogMaterial;
+
+    /// <summary>
+    /// Reference to the camera attached to this gameobject.
+    /// </summary>
+    public Camera _camera;
+
+    /// <summary>
+    /// Flag for if the fog is enabled or not.
+    /// </summary>
+    private bool _enabled;
+
+    private void Start() {
+        _camera = GetComponent<Camera>();
+        if (_camera == null) {
+            Debug.LogWarning("Couldn't find a camera attached with this Fog component.");
+        }
+
+        // Enable generation of a depth texture
+        _camera.depthTextureMode = DepthTextureMode.Depth;
+    }
+
+    private void Update() {
+        // Toggling the fog effect
+        if (Input.GetKeyDown(KeyCode.F) || Input.GetKeyDown("joystick button 3")) {
+            ToggleFog();
+        }
+    }
+
+    /// <summary>
+    /// Toggles the fog on or off.
+    /// </summary>
+    private void ToggleFog() {
+        // Adjust sight radius
+        if (_enabled) {
+            _camera.farClipPlane = 100f;
+        } else {
+            _camera.farClipPlane = 5f;
+        }
+        
+        _enabled = !_enabled; // Toggle enabled flag
+    }
+
+    /// <summary>
+    /// Called after all images are rendered.
+    /// Used for postprocessing.
+    /// </summary>
+    /// <param name="source">The source render texture</param>
+    /// <param name="destination">The destination render texture</param>
+    private void OnRenderImage(RenderTexture source, RenderTexture destination) {
+        if (_enabled) {
+            // Apply fog material onto destination texture
+            Graphics.Blit(source, destination, fogMaterial);
+        } else {
+            Graphics.Blit(source, destination); // Default rendering
+        }
+    }
+}
