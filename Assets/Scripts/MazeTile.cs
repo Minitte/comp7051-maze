caa42359b79b8e426d6235606b91f88f5081be6d
Minitte <arcentalminitte@gmail.com>
2018-10-29 19:23:20 -0700

    toggle day night ambient light

--- Assets/Scripts/MazeTile.cs
+++ Assets/Scripts/MazeTile.cs
@@ -3,17 +3,60 @@
 /// This is a class for maze tiles.
 /// </summary>
 public class MazeTile : MonoBehaviour {
-
-
 	/// <summary>
 	/// list of walls of the tile
 	/// </summary>
 	public GameObject[] walls;
 
 	/// <summary>
+	/// The floor of the tile
+	/// </summary>
+	public GameObject floor;
+
+	/// <summary>
 	/// The coordinate of the tile.
 	/// </summary>
 	public Coordinate coord;
+
+	/// <summary>
+	/// Toggle for day light
+	/// </summary>
+	private bool _dayToggle;
+
+	/// <summary>
+	/// key cooldown
+	/// </summary>
+	private bool _cooldown;
+
+	/// <summary>
+	/// Update is called every frame, if the MonoBehaviour is enabled.
+	/// </summary>
+	void Update() {
+		float dni = Input.GetAxis("DayNightInput");
+		
+		if (_cooldown) {
+            if (dni > 0.1f) {
+                return;
+            } else {
+                _cooldown = false;
+            }
+        }
+
+		if (dni > 0.1f) {
+			_dayToggle = !_dayToggle;
+			_cooldown = true;
+
+			float light = _dayToggle ? 1f : 0.5f;
+
+			for (int i = 0; i < walls.Length; i++) {
+				if (walls[i] != null) {
+					walls[i].GetComponent<Renderer>().material.SetFloat("_AmbientLighIntensity", light);
+				}
+			}
+
+			floor.GetComponent<Renderer>().material.SetFloat("_AmbientLighIntensity", light);
+		}
+	}
 
 	/// <summary>
 	/// Breaks a wall in the input direction.
