f087ba70db9c3c45e6173c8021fccdba86ea9b92
Anthony Wong <awong6201@gmail.com>
2018-11-23 17:45:56 -0800

    Player can throw a ball in the direction of the camera
    Set physics material of ball to bounce easily
    Also put a limit on the ball's lifetime

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter(Collision collision) {
+
+    }
+}
---
+++ Assets/Scripts/BallThrower.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class BallThrower : MonoBehaviour {
+
+    /// <summary>
+    /// Reference to the ball prefab.
+    /// </summary>
+    public GameObject ballPrefab;
+
+    /// <summary>
+    /// The amount of force to put on the ball when thrown.
+    /// </summary>
+    public float force;
+
+	// Update is called once per frame
+	private void Update () {
+		if (Input.GetKeyDown(KeyCode.Mouse1)) {
+            ThrowBall();
+        }
+	}
+
+    /// <summary>
+    /// Throws the ball.
+    /// </summary>
+    private void ThrowBall() {
+        // Instantiate ball object and ignore collisions with the player
+        GameObject ball = Instantiate(ballPrefab);
+        Physics.IgnoreCollision(ball.GetComponent<Collider>(), GetComponent<CharacterController>());
+
+        // Place ball at player's position, and throw it in direction of camera
+        ball.transform.position = transform.position;
+        Vector3 direction = GetComponent<Player>().fpsCamera.transform.forward;
+        ball.GetComponent<Rigidbody>().AddForce(direction * force);
+    }
+}
