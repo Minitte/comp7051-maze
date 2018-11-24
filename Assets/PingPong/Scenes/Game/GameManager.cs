ed1617e4c06272aca3e1a3e2db6a26da8bdca116
Minitte <arcentalminitte@gmail.com>
2018-11-24 12:27:18 -0800

    fixed inputs and enabled AI for p2

--- Assets/PingPong/Scenes/Game/GameManager.cs
+++ Assets/PingPong/Scenes/Game/GameManager.cs
@@ -68,8 +68,15 @@ public class GameManager : MonoBehaviour {
 
 		// subscribe to event
 		GameEventManager.OnGoal += SpawnBallOnGoal;
+	}
 
-		SpawnBall();
+	/// <summary>
+	/// Start is called on the frame when a script is enabled just before
+	/// any of the Update methods is called the first time.
+	/// </summary>
+	void Start()
+	{
+		SpawnBall();	
 	}
 
 	/// <summary>
