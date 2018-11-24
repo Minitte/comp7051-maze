0bcd600623537300d16d9ce3d2c8ab4ea48fd811
Anthony Wong <awong6201@gmail.com>
2018-11-23 18:00:32 -0800

    Ball plays sound effect on collisions

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -2,16 +2,21 @@
 
 public class Ball : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-    }
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    /// <summary>
+    /// The sound effect to play when this ball collides with something.
+    /// </summary>
+    public AudioClip soundFX;
 
+    /// <summary>
+    /// The audio source of the ball.
+    /// </summary>
+    public AudioSource audioSource;
+
+    /// <summary>
+    /// Called when this gameobject's collider hits another collider.
+    /// </summary>
+    /// <param name="collision">The other collider involved in the collision</param>
     private void OnCollisionEnter(Collision collision) {
-
+        SoundManager.instance.PlaySound(audioSource, soundFX); // Play sound effect
     }
 }
